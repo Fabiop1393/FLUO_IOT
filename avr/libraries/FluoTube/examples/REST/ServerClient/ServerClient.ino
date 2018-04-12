b469d2c525fdb18931816e327b25ca4ae3819e2d
fabio <pupillofabio93@gmail.com>
2018-04-12 15:20:51 +0200

    esempio REST aggiornato con websocket

--- avr/libraries/FluoTube/examples/REST/ServerClient/ServerClient.ino
+++ avr/libraries/FluoTube/examples/REST/ServerClient/ServerClient.ino
@@ -1,38 +1,51 @@
-#include <FluoTube.h>
+#include<FluoTube.h>             
+
+unsigned long t1;
+unsigned long t2;
+int i = 0;
+int spam = 0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);         
   Serial.println("AVR booting ...");
 
   pinMode(13, OUTPUT);
-  
-  FluoTube.Begin();
-  
+  FluoTube.Begin();             
   FluoTube.WiFi.Setting("Androidadry", "hellohello", "fluo"); //Set WiFi (ssid, passkey, hostname)
-  FluoTube.WiFi.ServerRest(); // enable Rest Server default port 8080
+  FluoTube.WiFi.ServerRest();   // enable Rest Server default port 8080
+
+  t1 = millis();
 }
 
 void loop() {
-  FluoTube.Run();
-
-  if ( FluoTube.WiFi.Available() )
-    Parser ( FluoTube.WiFi.RestRead() ); 
-}
-
-void Parser (String data)
-{
-  if (data == "/l13on")
-  {
-    digitalWrite(13, HIGH);
-  }   
-  if (data == "/l13off")
-  {
-    digitalWrite(13, LOW);
+ 
+  FluoTube.Run();                 
+  if (FluoTube.WiFi.Available()) { 
+    Parser(FluoTube.WiFi.RestRead()); 
+    if(spam > 0){
+      t2 = millis();
+      if( (t2-t1) > 15000){        
+        FluoTube.sendMsgToESP("Hi, I'm AVR and I sent  "+ String(spam)+" messages");
+        spam++;
+        t1 = millis();
+      }
+    }
+    
   }
   
-  if(data == "ADDR") //default handle to show IP ADDR!!!
-  {
-    Serial.println("Inviare Richieste REST all indirizzo: ");
-    Serial.println( FluoTube.WiFi.Info() );
+}
+void Parser(String data) {            
+  if (data == "/l13on") {             
+    digitalWrite(13, HIGH);
+    FluoTube.sendMsgToESP("Led 13 ON");
+    spam = 1;
+  } if (data == "/l13off") {
+    digitalWrite(13, LOW);
+    FluoTube.sendMsgToESP("Led 13 OFF");
+    spam = 1;
+  } if (data == "ADRR") {
+    Serial.println("Inviare Richieste Rest all'indirizzo: ");  
+    Serial.println(FluoTube.WiFi.Info());
+    spam = 1; 
   }
 }
